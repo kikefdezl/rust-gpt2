c40717e85e24a6fc61cb43e66c789c501e9ca856
kike <enriquelagui@outlook.com>
2025-08-07 19:46:55 +0200

    add third party openai bpe encoder

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
+use bpe_openai::cl100k_base;
 use llm_from_scratch::config;
-use llm_from_scratch::tokenizer::{END_OF_TEXT_TOKEN, SimpleTokenizer, build_vocabulary, tokenize};
+use llm_from_scratch::tokenizer::{END_OF_TEXT_TOKEN, build_vocabulary, tokenize};
 
 use std::fs::read_to_string;
 
@@ -7,12 +8,14 @@ fn main() {
     let text = read_to_string(config::RAW_DATA_FILE).unwrap();
     let tokens = tokenize(&text);
     let vocabulary = build_vocabulary(tokens);
-    let tokenizer = SimpleTokenizer::from_vocabulary(&vocabulary);
+
+    let tokenizer = cl100k_base();
     let text1 = "Hello, do you like tea?";
     let text2 = "In the sunlit terraces of the palace.";
     let text = [text1, text2].join(&format!(" {END_OF_TEXT_TOKEN} "));
     let ids = tokenizer.encode(&text);
 
-    let text = tokenizer.decode(&ids);
+    println!("{ids:?}");
+    let text = tokenizer.decode(&ids).unwrap();
     println!("{text}");
 }
